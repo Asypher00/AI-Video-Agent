8c57ab7f0982e873e2908de47d1d31420e2798dd
Neeraj Agrawal <shashineerajagrawal@gamil.com>
2025-06-30 17:14:33 +0530

    Made tailwind changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ManufacturingInspector from './ManufacturingInspector'
-// import "./index.css" ;
+import "./index.css" ;
 
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default  {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // This is key! It covers most React setups.
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
